9ca633d41050947a83f93f99febba9a1a77fc80e
Noah Manneschmidt <noah@manneschmidt.net>
2010-08-29 00:21:04 -0500

    Switched to ruby 1.8 syntax

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,5 @@
 class UserController < ApplicationController
-  before_filter :login_required, only: :change_password
+  before_filter :login_required, only => :change_password
   
   def login
     if session[:user]
@@ -17,13 +17,13 @@ class UserController < ApplicationController
   def logout
     session[:user] = nil
     flash[:message] = 'Logged out'
-    redirect_to action: 'login'
+    redirect_to action => 'login'
   end
     # 
     # def change_password
     #   @user = session[:user]
     #   if request.post?
-    #     @user.update_attributes(password: params[:password], password_confirmation: params[:password_confirmation])
+    #     @user.update_attributes(password: params[:password], password_confirmation => params[:password_confirmation])
     #     if @user.save
     #       flash[:message] = "Password changed"
     #     end
